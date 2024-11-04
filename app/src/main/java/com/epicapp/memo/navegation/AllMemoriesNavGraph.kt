67c9a6c3c969644d5d02de0ba903d0b04e466639
Qu3zada22 <139565119+Qu3zada22@users.noreply.github.com>
2024-11-03 19:20:51 -0600

    vals

--- app/src/main/java/com/epicapp/memo/navegation/AllMemoriesNavGraph.kt
+++ app/src/main/java/com/epicapp/memo/navegation/AllMemoriesNavGraph.kt
@@ -9,8 +9,8 @@ import com.epicapp.memo.ui.allmemories.view.Memory
 fun NavGraphBuilder.allMemoriesNavGraph(navController: NavHostController) {
     composable("allMemories") {
         AllMemoriesScreen(
-            favoriteMemories = listOf(),
-            allMemories = listOf(),
+            favoriteMemories = favoriteMemories,
+            allMemories = allMemories,
             onMemoryClick = { memory -> navController.navigate("memoryView/${memory.id}") },
             onAddMemoryClick = { navController.navigate("menu") },
             onHeartClick = { navController.navigate("detailMemories") },
@@ -18,3 +18,20 @@ fun NavGraphBuilder.allMemoriesNavGraph(navController: NavHostController) {
         )
     }
 }
+val favoriteMemories = listOf(
+    Memory("1", "Favorite Memory 1", "Description 1", "https://via.placeholder.com/150", "Song 1", "2023-01-01"),
+    Memory("2", "Favorite Memory 2", "Description 2", "https://via.placeholder.com/150", "Song 2", "2023-01-02"),
+    Memory("3", "Favorite Memory 3", "Description 3", "https://via.placeholder.com/150", "Song 3", "2023-01-03"),
+    Memory("4", "Favorite Memory 4", "Description 4", "https://via.placeholder.com/150", "Song 4", "2023-01-04"),
+    Memory("5", "Favorite Memory 5", "Description 5", "https://via.placeholder.com/150", "Song 5", "2023-01-05"),
+    Memory("6", "Favorite Memory 6", "Description 6", "https://via.placeholder.com/150", "Song 6", "2023-01-06"),
+)
+
+val allMemories = listOf(
+    Memory("7", "Memory 1", "Description 1", "https://via.placeholder.com/150", "Song 1", "2023-01-01"),
+    Memory("8", "Memory 2", "Description 2", "https://via.placeholder.com/150", "Song 2", "2023-01-02"),
+    Memory("9", "Memory 3", "Description 3", "https://via.placeholder.com/150", "Song 3", "2023-01-03"),
+    Memory("10", "Memory 4", "Description 4", "https://via.placeholder.com/150", "Song 4", "2023-01-04"),
+    Memory("11", "Memory 5", "Description 5", "https://via.placeholder.com/150", "Song 5", "2023-01-05"),
+    Memory("12", "Memory 6", "Description 6", "https://via.placeholder.com/150", "Song 6", "2023-01-06"),
+)
